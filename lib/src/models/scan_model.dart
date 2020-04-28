fcb36b9ac150fef16123e9a013b99e449efa51c7
JCamiloo <jcamilo.osorio15@gmail.com>
2020-04-28 16:47:24 -0500

    scan model

---
+++ lib/src/models/scan_model.dart
@@ -0,0 +1,20 @@
+class Scan {
+
+  int id;
+  String type;
+  String value;
+
+  Scan({ this.id, this.type, this.value });
+
+  factory Scan.fromJson(Map<String, dynamic> json) => new Scan(
+    id: json['id'],
+    type: json['type'],
+    value: json['value']
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "type": type,
+    "value": value
+  };
+}
